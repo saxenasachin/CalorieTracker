8fe43e5548b6c17dec13f84ab83d679e608adc63
Sachin <sachin.saxena1790@gmail.com>
2023-12-19 23:41:42 +0530

    create tracker overview components

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -20,6 +20,7 @@ import com.example.onboarding_presentation.height.HeightScreen
 import com.example.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.example.onboarding_presentation.weight.WeightScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
+import com.example.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -82,7 +83,7 @@ class MainActivity : ComponentActivity() {
                             GoalScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.TRACKER_OVERVIEW) {
-
+                            TrackerOverviewScreen(onNavigate = navController::navigate)
                         }
                         composable(Route.SEARCH) {
 
---
+++ tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -0,0 +1,49 @@
+package com.example.tracker_presentation.tracker_overview
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.core.util.UiEvent
+import com.example.core_ui.LocalSpacing
+import com.example.tracker_presentation.components.DaySelector
+import com.example.tracker_presentation.tracker_overview.components.NutrientsHeader
+
+@Composable
+fun TrackerOverviewScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: TrackerOverviewViewModel = hiltViewModel()
+) {
+    val spacing = LocalSpacing.current
+    val state = viewModel.state
+    val context = LocalContext.current
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = spacing.spaceMedium)
+    ) {
+        item {
+            NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+    }
+}
